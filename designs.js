bd61f33e63b26f4b5c0af9c9c6b76397c14bc8f1
bd8239 <51523682+bd8239@users.noreply.github.com>
2019-07-01 22:48:14 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,41 @@
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(event) {
+  var sizeForm  = document.getElementById("sizePicker");
+  var cols = sizeForm.inputWidth.value;
+  var rows = sizeForm.inputHeight.value;
+  var pixelCanvas = document.getElementById("pixelCanvas");
+  injectSizedGrid(pixelCanvas, rows, cols);
+  event.preventDefault();
 }
+
+
+// removes any existing children theninjects the appropriate 
+// amount of rows and columns into the table
+function injectSizedGrid(tableNode, rows, cols) {
+
+  // first remove the existing rows and cols
+  while (tableNode.firstChild) {
+    tableNode.firstChild.remove();
+  }
+ 
+  for (row_index = 0; row_index < rows; row_index++) { 
+    row = tableNode.insertRow(row_index);
+    for (col_index = 0; col_index < cols; col_index++) { 
+      cell = row.insertCell(col_index);
+      cell.addEventListener(
+        'click', 
+        colorCell.bind(this, cell)
+      );
+    }
+  }
+}
+
+function colorCell(selectedCell) {
+  selectedColor = document.getElementById("colorPicker").value;
+  selectedCell.style.backgroundColor = selectedColor;
+}
+
+
+document.getElementById("sizePicker").addEventListener("submit", makeGrid, false);
